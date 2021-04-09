9e7cbc4bc2647a8a1d54d54153b9c44b9b27cb8d
Harshit Pandey <69616901+HarshitPandey251@users.noreply.github.com>
2021-04-09 19:30:44 +0530

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 module.exports={
-    MONGOURI: "mongodb+srv://Harshit:QrkNCxUobu3VjOv1@cluster0.krr7j.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    JWT_SECRET:"egdfkdhfsnn232"
+    MONGOURI: "",
+    JWT_SECRET:""
 }
